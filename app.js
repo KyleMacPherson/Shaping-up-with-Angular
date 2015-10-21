2f3498e349cb28e9b9dda3505ca53cc8e8c99fe3
kyle macpherson <kaimac2010@gmail.com>
2015-10-21 21:47:50 +0100

    directives added for soldOut and canPurchase, add to cart functionality added

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-var app = angular.module('store', [ ]);
+(function(){
+  var gem = {
+    name: 'Azurite', price: 2.95,
+    price: 110.50,
+    canPurchase: false,
+    soldOut: true
+  };
+
+  var app = angular.module('gemStore', []);
+
+  app.controller("StoreController", function () {
+  	this.product = gem;
+  });
+
+})();
